bf14e770bb8f52e8e5d135c624545600186a58a8
Johnpaul <polymenjohn1@gmail.com>
2023-12-04 17:59:50 +0100

    mongo setup

--- main.go
+++ main.go
@@ -1,24 +1,31 @@
 package main
 
 import (
+	// "context"
 	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/blockchain-hackers/indexer/functions"
+	"github.com/blockchain-hackers/indexer/database"
+	// "github.com/wailsapp/wails/lib/interfaces"
+	// "go.mongodb.org/mongo-driver/bson"
+	// "github.com/blockchain-hackers/indexer/functions"
 	"github.com/blockchain-hackers/indexer/triggers"
 	// "github.com/ethereum/go-ethereum/common"
 )
 
 func main() {
-	resp, _ := callFunc("transfer", functions.ProcessFunctionParams{
-		FunctionName: "transfer",
-		Parameters: map[string]interface{}{
-			"to":         "0x84188bc94B497131d0Ee2Cf7C154b22357c25208",
-			"amount":     int64(500000000000000),
-			"privateKey": "6e1b485777de659f004d1133e422def4be77d0346716e65278a369c9eb9d544b",
-		}})
-	fmt.Printf("Response: %+v\n", resp.Value)
+	// resp, _ := callFunc("transfer", functions.ProcessFunctionParams{
+	// 	FunctionName: "transfer",
+	// 	Parameters: map[string]interface{}{
+	// 		"to":         "0x84188bc94B497131d0Ee2Cf7C154b22357c25208",
+	// 		"amount":     int64(500000000000000),
+	// 		"privateKey": "6e1b485777de659f004d1133e422def4be77d0346716e65278a369c9eb9d544b",
+	// 	}})
+	// fmt.Printf("Response: %+v\n", resp.Value)
+	DBClient := database.Connect()
+	// dbs,_ := DBClient.ListDatabaseNames(context.Background(), bson.D{{}})
+	fmt.Printf("Database connected successfully: %+v\n", (DBClient.NumberSessionsInProgress()))
 	triggers.Run()
 
 	// Simple HTTP server
