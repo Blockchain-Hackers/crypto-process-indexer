85f020b36d957bcbebcc75a8f74e1fd116b62319
Davyking <yungdynamic53@gmail.com>
2023-12-05 05:47:35 -0800

    calling ccip

--- ccip/ccip-trans.go
+++ ccip/ccip-trans.go
@@ -1,40 +1,430 @@
-// make function that transfer token cross chain, 
+package triggers
 
-package main
+// package main
 
 import (
-    // Import necessary libraries for Ethereum and Binance Smart Chain
-    "github.com/ethereum/go-ethereum/ethclient"
-    // Other necessary imports
+	"context"
+	"fmt"
+	"log"
+	"strings"
 
-    // Import necessary libraries for Binance Smart Chain
-    "github.com/binance/chain-go-sdk/client"
-    // Other necessary imports
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func transferTokensAcrossChains() {
-    // Connect to Ethereum
-    ethClient, err := ethclient.Dial("https://eth-node-url")
-    if err != nil {
-        // Handle connection error
-        return
-    }
+var ChainlinkCCIPAbi = `[
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_router",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_link",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint64",
+				"name": "destinationChainSelector",
+				"type": "uint64"
+			}
+		],
+		"name": "DestinationChainNotAllowlisted",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "target",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "FailedToWithdrawEth",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "currentBalance",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "calculatedFees",
+				"type": "uint256"
+			}
+		],
+		"name": "NotEnoughBalance",
+		"type": "error"
+	},
+	{
+		"inputs": [],
+		"name": "NothingToWithdraw",
+		"type": "error"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferRequested",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "bytes32",
+				"name": "messageId",
+				"type": "bytes32"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint64",
+				"name": "destinationChainSelector",
+				"type": "uint64"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "receiver",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "token",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokenAmount",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "feeToken",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "fees",
+				"type": "uint256"
+			}
+		],
+		"name": "TokensTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "acceptOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint64",
+				"name": "_destinationChainSelector",
+				"type": "uint64"
+			},
+			{
+				"internalType": "bool",
+				"name": "allowed",
+				"type": "bool"
+			}
+		],
+		"name": "allowlistDestinationChain",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint64",
+				"name": "",
+				"type": "uint64"
+			}
+		],
+		"name": "allowlistedChains",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint64",
+				"name": "_destinationChainSelector",
+				"type": "uint64"
+			},
+			{
+				"internalType": "address",
+				"name": "_receiver",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_token",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transferTokensPayLINK",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "messageId",
+				"type": "bytes32"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint64",
+				"name": "_destinationChainSelector",
+				"type": "uint64"
+			},
+			{
+				"internalType": "address",
+				"name": "_receiver",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_token",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transferTokensPayNative",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "messageId",
+				"type": "bytes32"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_beneficiary",
+				"type": "address"
+			}
+		],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_beneficiary",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_token",
+				"type": "address"
+			}
+		],
+		"name": "withdrawToken",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	}
+]`
 
-    // Connect to Binance Smart Chain
-    binanceClient := client.NewDexClient("https://binance-node-url")
-    // Authenticate and set up client for Binance Smart Chain
-    // Handle authentication errors
+// const infuraURL = "wss://sepolia.infura.io/ws/v3/927b0bef549145fba75661d347f23b8a"
 
-    // Approve tokens on Ethereum
-    // Call the approve function of the ERC-20 token contract on Ethereum to approve token transfer
-
-    // Use a bridge or custodian service to trigger the transfer from Ethereum to Binance Smart Chain
-
-    // Mint tokens on Binance Smart Chain
-    // Call the deposit or mint function on the contract to receive tokens on Binance Smart Chain
+type CCIPTransferInfo struct {
+	amount string
+	address string
+	senderChainId string
+	receiverChainId string
+	useLink bool
 }
 
-func main() {
-    // Transfer tokens across chains
-    transferTokensAcrossChains()
+
+
+type CCIPResponse struct {
+	msg string
 }
+
+
+
+func transferToken(ccipInfo CCIPTransferInfo) (*CCIPResponse, error) {
+	rpcUrl := ""
+	client, err := ethclient.Dial(rpcUrl)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to Ethereum client: %v", err)
+	}
+	defer client.Close()
+
+	sendContractAddr := ""
+
+	contractAddress := common.HexToAddress(sendContractAddr)
+	parsedABI, err := abi.JSON(strings.NewReader(ChainlinkCCIPAbi))
+	// fmt.Printf("Parsed ABI: %v", parsedABI)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse contract ABI: %v", err)
+	}
+
+
+	instance, err := store.NewStore(address, client)
+
+	// Load the contract instance
+	contractInstance, err := NewContract(contractAddress, client)
+	if err != nil {
+		log.Fatalf("Failed to instantiate contract: %v", err)
+	}
+
+
+	if (ccipInfo.useLink){
+		query := ethereum.CallMsg{
+			To:   &contractAddress,
+			Data: parsedABI.Methods["transferTokensPayLINK"].ID,
+		}
+	} else {
+		query := ethereum.CallMsg{
+			To:   &contractAddress,
+			Data: parsedABI.Methods["transferTokensPayNative"].ID,
+		}
+	}
+
+	
+
+	result, err := client.CallContract(context.Background(), query, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to call contract: %v", err)
+	}
+	// log result
+	// log.Printf("Result: %v", result)
+
+	// var answer big.Int
+	var data, _ = parsedABI.Unpack("latestRoundData", result)
+	log.Printf("Data: %v", data)
+	var answer2 ChainlinkLatestRoundData
+	err = parsedABI.UnpackIntoInterface(&answer2, "latestRoundData", result)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unpack contract result: %v", err)
+	}
+
+	return &answer2, nil
+}
+
+// func main() {
+// 	trigger := &ChainlinkPriceFeed{}
+// 	trigger.run()
+// }
